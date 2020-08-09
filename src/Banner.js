3205def4bc8f90113e2b221d9e5373e8b61d7887
Saul Corona <saulcoronasantos@ymail.com>
2020-08-09 13:55:06 -0500

    Add mpvie popup

--- src/Banner.js
+++ src/Banner.js
@@ -21,32 +21,37 @@ function Banner() {
         return str ?. length > n ? str.substr(0, n - 1) + "..." : str;
     }
 
-    return (<header className="banner"
-        style={
-            {
-                backgroundSize: "cover",
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${
-                    movie ?. backdrop_path
-                }")`,
-                backgroundPosition: "initial"
-            }
-    }>
-        <div className="banner__content">
-            <h1 className="banner__title"> {
-                movie ?. title || movie ?. name || movie ?. original_name
-            }</h1>
+    return (
+        <header className="banner"
+            style={
+                {
+                    backgroundSize: "cover",
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${
+                        movie ?. backdrop_path
+                    }")`,
+                    backgroundPosition: "initial"
+                }
+        }>
+            <div className="banner--fadeUp"></div>
+            <div className="banner__content">
+                <h1 className="banner__title">
+                    {
+                    movie ?. title || movie ?. name || movie ?. original_name
+                }</h1>
 
-            <div className="banner__buttons">
-                <button className="banner__button">Play</button>
-                <button className="banner__button">My List</button>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+
+                <h1 className="banner__description">
+                    {
+                    truncate(movie ?. overview, 150)
+                }</h1>
             </div>
-
-            <h1 className="banner__description"> {
-                truncate(movie ?. overview, 150)
-            }</h1>
-        </div>
-        <div className="banner--fadeBottom"></div>
-    </header>)
+            <div className="banner--fadeBottom"></div>
+        </header>
+    )
 }
 
 export default Banner
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 /*Importacion de paquetes */
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import Youtube from 'react-youtube';
+import movieTrailer from "movie-trailer";
 import "./Row.css"
 
 /*Variables / contantes globales */
@@ -8,6 +10,7 @@ const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({title, fetchUrl, isLargeRow}) { // Funcion principal
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
     // A snipet of code wich runs based on a specific condition/variable
     useEffect(() => { // if [], run once when the row loads, and dont run again
         async function fetchData() {
@@ -18,29 +21,58 @@ function Row({title, fetchUrl, isLargeRow}) { // Funcion principal
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+            autoplay: 1
+        }
+    };
 
-    return (<div className="row">
-        <h2> {title}</h2>
-        <div className="row__posters"> {/*container -> poster */}
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie ?. name || "").then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error))
+        }
+    }
+
+    return (
+        <div className="row">
+            <h2> {title}</h2>
+            <div className="row__posters">
+                {/*container -> poster */}
+                {
+                movies.map((movie) => (
+                    <img key={
+                            movie.id
+                        }
+                        onClick={
+                            () => handleClick(movie)
+                        }
+                        className={
+                            `row__poster ${
+                                isLargeRow && "row__posterLarge"
+                            }`
+                        }
+                        src={
+                            `${base_url}${
+                                isLargeRow ? movie.poster_path : movie.backdrop_path
+                            }`
+                        }
+                        alt={
+                            movie.name
+                        }/>
+                ))
+            } </div>
             {
-            movies.map(movie => (<img key={
-                    movie.id
-                }
-                className={
-                    `row__poster ${
-                        isLargeRow && "row__posterLarge"
-                    }`
-                }
-                src={
-                    `${base_url}${
-                        isLargeRow ? movie.poster_path : movie.backdrop_path
-                    }`
-                }
-                alt={
-                    movie.name
-                }/>))
+            trailerUrl && <Youtube videoId={trailerUrl}
+                opts={opts}/>
         } </div>
-    </div>)
+    )
 }
 
 export default Row
